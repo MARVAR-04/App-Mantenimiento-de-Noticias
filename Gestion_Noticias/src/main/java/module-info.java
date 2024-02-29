011b07a6dc070fc26de64cefbfa79657f423db7c
MARVAR <134282944+MARVAR-04@users.noreply.github.com>
2024-02-29 20:51:59 +0100

    Delete Gestion_Noticias directory

--- Gestion_Noticias/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.gestion_noticias {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.gestion_noticias to javafx.fxml;
-    exports com.example.gestion_noticias;
-}
